7d2f70b37a8492c1fde6cbc7a09116a7d5f1ced4
disha00991 <directionstar86@gmail.com>
2021-07-23 17:17:15 -0400

    removed cross orgin

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Importing essential libraries
 from flask import Flask, request, make_response, jsonify
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 from color_palette_creation import get_palette_plot
 from PIL import Image
 from io import BytesIO
@@ -8,7 +8,7 @@ import requests
 from icecream import ic
 
 app = Flask(__name__)
-
+CORS(app)
 """
 @todo
 input other algorithm for clustering @todo
@@ -16,16 +16,13 @@ send rgb color values for use by user @todo
 home end point here -> deployed frontend link
 image can be uploaded from frontend too
 """
-@app.route('/get_color_palette', methods=['POST','GET', 'OPTIONS'])
+@app.route('/get_color_palette', methods=['POST','GET'])
 def color_palette():
     is_url = request.args['is_url']
     algo = request.args['algo']
     url = request.args['url']
     n_colors = request.args['n_colors']
     print("partt")
-    if request.method == "OPTIONS": # CORS preflight
-        print('preflight')
-        return _build_cors_prelight_response()
     if request.method == 'POST' or request.method == 'GET':
         img = ''
         if (is_url=='true' and url):
@@ -33,18 +30,7 @@ def color_palette():
         # else:
         #     img = Image.open(request.files['file'])
 
-    return _corsify_actual_response(jsonify(get_palette_plot(img, algo, int(n_colors))))
-
-def _build_cors_prelight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+    return get_palette_plot(img, algo, int(n_colors))
 
 @app.route('/')
 def home_endpoint():
