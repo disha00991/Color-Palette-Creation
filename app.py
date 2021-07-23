5e7b0e98e7c8432945f865408a8f0a4f8ea75274
disha00991 <directionstar86@gmail.com>
2021-07-23 16:41:54 -0400

    added cross orign

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Importing essential libraries
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
+from flask import Flask, request, make_response, jsonify
+# from flask_cors import CORS, cross_origin
 from color_palette_creation import get_palette_plot
 from PIL import Image
 from io import BytesIO
@@ -8,8 +8,6 @@ import requests
 from icecream import ic
 
 app = Flask(__name__)
-CORS(app, resources={r"/get_color_palette": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 """
 @todo
@@ -18,14 +16,16 @@ send rgb color values for use by user @todo
 home end point here -> deployed frontend link
 image can be uploaded from frontend too
 """
-@app.route('/get_color_palette', methods=['POST','GET'])
-@cross_origin()
+@app.route('/get_color_palette', methods=['POST','GET', 'OPTIONS'])
 def color_palette():
     is_url = request.args['is_url']
     algo = request.args['algo']
     url = request.args['url']
     n_colors = request.args['n_colors']
     print("partt")
+    if request.method == "OPTIONS": # CORS preflight
+        print('preflight')
+        return _build_cors_prelight_response()
     if request.method == 'POST' or request.method == 'GET':
         img = ''
         if (is_url=='true' and url):
@@ -33,10 +33,20 @@ def color_palette():
         # else:
         #     img = Image.open(request.files['file'])
 
-    return get_palette_plot(img, algo, int(n_colors))
+    return _corsify_actual_response(jsonify(get_palette_plot(img, algo, int(n_colors))))
+
+def _build_cors_prelight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 @app.route('/')
-@cross_origin()
 def home_endpoint():
     return "<h1>This is the Flask app for Color Palette Creation Project hosted at <a href='https://disha00991.github.io/webume/#/projects/color-palette'>here</a></h1>"
 
