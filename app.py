df746107d3605633158c4b75120e2104955638c0
disha00991 <directionstar86@gmail.com>
2021-07-24 19:31:00 -0400

    added birch and agglo clustering

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Importing essential libraries
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS, cross_origin
-from color_palette_creation import get_palette_plot
+from clustering_algos import Clustering_algos
 from PIL import Image
 from io import BytesIO
 import requests
@@ -22,19 +22,21 @@ def color_palette():
     algo = request.args['algo']
     url = request.args['url']
     n_colors = request.args['n_colors']
-    print("partt")
     if request.method == 'POST' or request.method == 'GET':
         img = ''
         if (is_url=='true' and url):
             img = Image.open(BytesIO(requests.get(url).content))
-        # else:
-        #     img = Image.open(request.files['file'])
 
-    return get_palette_plot(img, algo, int(n_colors))
+    cluster_obj = Clustering_algos(img, algo, int(n_colors))
+    return cluster_obj.get_palette_plot()
 
 @app.route('/')
 def home_endpoint():
     return "<h1>This is the Flask app for Color Palette Creation Project hosted at <a href='https://disha00991.github.io/webume/#/projects/color-palette'>here</a></h1>"
+
+@app.route('/get_color_palette')
+def get_color_palette_endpoint():
+    return "<h1>Use algo, url and no of colors in Query Params to get color palette</h1>"
 
 if __name__ == '__main__':
     app.run()
